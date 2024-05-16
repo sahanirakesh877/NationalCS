fa43831bd468d5d6e7dc052c655a76f5c48c08c2
sudhirayadav11 <sudhirayadav11@gmail.com>
2024-05-16 17:52:39 +0545

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import About from "./Pages/About";
@@ -13,24 +13,40 @@ import Footer from "./Components/Footer";
 import DoctorDetails from "./Pages/DoctorDetails";
 import Appointment from "./Pages/Appointment";
 import Case from "./Pages/Case";
+import Loader from "./Components/Loader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+useEffect(()=>{
+  setTimeout(()=>{
+    setLoading(false);
+  },1000)
+})
+
   return (
     <>
-      <TopBar />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/service/:id" element={<ServiceDetail />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/doctor" element={<Doctor />} />
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="/case" element={<Case />} />
-        <Route path="/doctors/:id" element={<DoctorDetails />} />
-      </Routes>
-      <Footer />
+      {loading ? (
+        <>
+          <Loader />
+        </>
+      ) : (
+        <>
+          <TopBar />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/service/:id" element={<ServiceDetail />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/doctor" element={<Doctor />} />
+            <Route path="/appointment" element={<Appointment />} />
+            <Route path="/case" element={<Case />} />
+            <Route path="/doctors/:id" element={<DoctorDetails />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <>
+<div classname="d-flex justify-content-center align-items-center loader">
+Loding text.....
+  <div classname="rounded-circle h-20 w-20 bg-primary animate__animated animate__ping" />
+</div>
+
+
+    </>
+  )
+}
+
+export default Loader
